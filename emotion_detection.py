a127c0654140166da551a9852b5b59c1d38e7b4c
safiurrrehman <srehman.bese18seecs@seecs.edu.pk>
2020-11-03 23:23:18 +0500

    Updated

--- emotion_detection.py
+++ emotion_detection.py
@@ -5,43 +5,32 @@ from time import sleep
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing import image
 
-face_classifier = cv2.CascadeClassifier('/Users/safiurrehman/Desktop/Emotion Detection DL/haarcascade_frontalface_default.xml')
+faceCascade= cv2.CascadeClassifier('/Users/safiurrehman/Desktop/Emotion Detection DL/haarcascade_frontalface_default.xml')
 classifier =load_model('/Users/safiurrehman/Desktop/Emotion Detection DL/Emotion_saved.h5')
 
-class_labels = ['Angry','Happy','Neutral','Sad','Surprise']
+emotions = ['Angry','Happy','Neutral','Sad','Surprise']
 
 cap = cv2.VideoCapture(0)
 
 
 
 while True:
-    # Grab a single frame of video
     ret, frame = cap.read()
-    labels = []
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    faces = face_classifier.detectMultiScale(gray,1.3,5)
+    img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(img,1.3,5)
 
     for (x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h,x:x+w]
+        roi_gray = img[y:y+h,x:x+w]
         roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
-    # rect,face,image = face_detector(frame)
+        pixel = image.img_to_array(roi_gray)
+        pixel = np.expand_dims(pixel, axis = 0)  
+        pixel /= 255  
+        preds = classifier.predict(pixel)
+        label = int(np.argmax(preds[0]))
+        cv2.putText(frame, emotions[label], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-
-        if np.sum([roi_gray])!=0:
-            roi = roi_gray.astype('float')/255.0
-            roi = img_to_array(roi)
-            roi = np.expand_dims(roi,axis=0)
-
-        # make a prediction on the ROI, then lookup the class
-
-            preds = classifier.predict(roi)[0]
-            label=class_labels[preds.argmax()]
-            label_position = (x,y)
-            cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
-        else:
-            cv2.putText(frame,'No Face Found',(20,60),cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
-    cv2.imshow('Emotion Detector',frame)
+    cv2.imshow('Video', cv2.resize(frame,(1600,960),interpolation = cv2.INTER_CUBIC))
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
